18eccbeae3115f6e07efd8a889da512d13e17b53
Kim Rae Young <raeyoung233@gmail.com>
2020-10-19 22:25:50 +0900

    비디오 썸네일 생성

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
 import Axios from "axios";
 
@@ -8,87 +8,113 @@ const { Title } = Typography;
 
 const PriavteOptions = [
   { value: 0, label: "Private" },
-  { value: 1, label: "Public" }
-]
+  { value: 1, label: "Public" },
+];
 
 const CategoryOptions = [
   { value: 0, label: "Film & Animation" },
   { value: 1, label: "Autos & Venhicles" },
   { value: 2, label: "Music" },
-  { value: 3, label: "Pets & Animals" }
-]
+  { value: 3, label: "Pets & Animals" },
+];
 
 function VideoUploadPage() {
-
-  const [VideoTitle, setVideoTitle] = useState("")
-  const [Description, setDescription] = useState("")
-  const [Priavte, setPriavte] = useState(0) // priavte 0 , public 1
-  const [Category, setCategory] = useState("Film & Animation")
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Priavte, setPriavte] = useState(0); // priavte 0 , public 1
+  const [Category, setCategory] = useState("Film & Animation");
+  const [FilePath, setFilePath] = useState("");
+  const [Duration, setDuration] = useState("");
+  const [ThumbnailPath, setThumbnailPath] = useState("");
 
   const onTitleChange = (e) => {
-    setVideoTitle(e.currentTarget.value)
-  }
+    setVideoTitle(e.currentTarget.value);
+  };
 
   const onDescriptionChange = (e) => {
-    setDescription(e.currentTarget.value)
-  }
+    setDescription(e.currentTarget.value);
+  };
 
   const onPrivateChange = (e) => {
-    setPriavte(e.currentTarget.value)
-  }
+    setPriavte(e.currentTarget.value);
+  };
 
   const onCategoryChange = (e) => {
-    setCategory(e.currentTarget.value)
-  }
+    setCategory(e.currentTarget.value);
+  };
 
   const onDrop = (files) => {
-
     let formData = new FormData();
     const config = {
-      header: { 'content-type': 'multipart/form-data' }
-    }
-    formData.append("file", files[0])
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
     console.log(files);
 
-    Axios.post('/api/video/uploadfiles', formData, config).then(response => {
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
       if (response.data.success) {
         console.log(response.data);
+
+        let variable = {
+          url: response.data.url,
+          fileName: response.data.fileName,
+        };
+
+        setFilePath(response.data.url);
+
+        Axios.post("/api/video/thumbnail", variable).then((response) => {
+          if (response.data.success) {
+            console.log(response.data);
+
+            setDuration(response.data.fileDuration);
+            setThumbnailPath(response.data.url);
+          } else {
+            alert("Thumbnail generation failed");
+          }
+        });
       } else {
-        alert('Video Upload Fail!');
+        alert("Video Upload Fail!");
       }
-    })
-  }
+    });
+  };
   return (
-    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}>Upload Video</Title>
       </div>
-      <Form onSubmit>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Form>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
 
-          <Dropzone
-            onDrop={onDrop}
-            multiple={false}
-            maxSize={100000000}
-          >
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={100000000}>
             {({ getRootProps, getInputProps }) => (
-              <div style={{
-                width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex',
-                alignItems: 'center', justifyContent: 'center'
-              }} {...getRootProps()}>
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
                 <input {...getInputProps()} />
-                <Icon type="plus" style={{ fontSize: '3rem' }} />
-
+                <Icon type="plus" style={{ fontSize: "3rem" }} />
               </div>
             )}
           </Dropzone>
 
           {/* Thumbnail */}
-          <div>
-            <img src alt />
-          </div>
 
+          {ThumbnailPath && (
+            <div>
+              <img
+                src={`http://localhost:5000/${ThumbnailPath}`}
+                alt="thumbnail"
+              />
+            </div>
+          )}
         </div>
         <br />
         <br />
@@ -102,7 +128,9 @@ function VideoUploadPage() {
         <br />
         <select onChange={onPrivateChange}>
           {PriavteOptions.map((item, index) => (
-            <option key={index} value={item.value}>{item.label}</option>
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
           ))}
         </select>
 
@@ -110,19 +138,22 @@ function VideoUploadPage() {
         <br />
         <select onChange={onCategoryChange}>
           {CategoryOptions.map((item, index) => (
-            <option key={index} value={item.value}>{item.label}</option>
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
           ))}
         </select>
 
         <br />
         <br />
-        <Button type='primary' size="large" onClick>
-          Submit
-        </Button>
+        {
+          <Button type="primary" size="large" onClick>
+            Submit
+          </Button>
+        }
       </Form>
     </div>
-  )
-
+  );
 }
 
 export default VideoUploadPage;
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 //const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
+var ffmpeg = require("fluent-ffmpeg");
 
 // STORAGE MULTER CONFIG
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_${file.originalname}`);
-    },
-    fileFilter: (req, file, cb) => {
-        const ext = path.extname(file.originalname)
-        if (ext !== '.mp4') {
-            return cb(res.status(400).end('only mp4 is allowed'), false);
-        }
-        cb(null, true)
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== ".mp4") {
+      return cb(res.status(400).end("only mp4 is allowed"), false);
     }
-})
+    cb(null, true);
+  },
+});
 
 const upload = multer({ storage: storage }).single("file");
 
@@ -28,15 +29,56 @@ const upload = multer({ storage: storage }).single("file");
 //             Video
 //=================================
 
-router.post('/uploadfiles', (req, res) => {
+router.post("/uploadfiles", (req, res) => {
+  // 클라이언트에서 받은 비디오를 서버에 저장한다.
+  upload(req, res, (err) => {
+    if (err) {
+      return res.josn({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      url: res.req.file.path,
+      filename: res.req.file.filename,
+    });
+  });
+});
 
-    // 클라이언트에서 받은 비디오를 서버에 저장한다.
-    upload(req, res, err => {
-        if (err) {
-            return res.josn({ success: false, err })
-        }
-        return res.json({ success: true, url: res.req.file.path, filename: res.req.file.filename })
+router.post("/thumbnail", (req, res) => {
+  let filePath = "";
+  let fileDuration = "";
+
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+
+    fileDuration = metadata.format.duration;
+  });
+
+  ffmpeg(req.body.url)
+    .on("filenames", function (filenames) {
+      console.log("Will generate " + filenames.join(", "));
+      filePath = "uploads/thumbnails/" + filenames[0];
     })
-})
+    .on("end", function () {
+      console.log("Screenshots taken");
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .on("error", function () {
+      console.error(err);
+      return res.josn({ success: false, err });
+    })
+    .screenshots({
+      // Will take screens at 20%, 40%, 60% and 80% of the video
+      count: 3,
+      folder: "uploads/thumbnails",
+      size: "320x240",
+      // %b input basename ( filename w/o extension )
+      filename: "thumbnail-%b.png",
+    });
+});
 
 module.exports = router;
