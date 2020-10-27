d37e2e0121b87538a6d15efe9aea83055a86b25c
Kim Rae Young <raeyoung233@gmail.com>
2020-10-27 23:44:21 +0900

    create subscription page

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Video } = require("../models/Video");
+const { Subscriber } = require("../models/Subscriber");
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
@@ -110,6 +111,30 @@ router.post("/thumbnail", (req, res) => {
       // %b input basename ( filename w/o extension )
       filename: "thumbnail-%b.png",
     });
+});
+
+router.post("/getSubscriptionVideos", (req, res) => {
+  // 자신의 아이디를 가지고 구독하는 사람 찾기
+  Subscriber.find({ userFrom: req.body.userFrom }).exec(
+    (err, subscriberInfo) => {
+      if (err) return res.status(400).send(err);
+
+      let subscribedUser = [];
+
+      subscriberInfo.map((subscriber, i) => {
+        subscribedUser.push(subscriber.userTo);
+      });
+
+      // 구독하는 사람들의 비디오 불러오기
+      // $in : mongoDB 기능 활용 -> 다수의 데이터인 경우 모든 정보 불러오기
+      Video.find({ writer: { $in: subscribedUser } })
+        .populate("writer")
+        .exec((err, videos) => {
+          if (err) return res.status(400).send(err);
+          res.status(200).json({ success: true, videos });
+        });
+    }
+  );
 });
 
 module.exports = router;
