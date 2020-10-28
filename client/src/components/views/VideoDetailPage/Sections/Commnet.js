13f2efe3b86827356e5fcb54d50c13d0745b8c4d
Kim Rae Young <raeyoung233@gmail.com>
2020-10-28 22:21:01 +0900

    Create Comment Structure

---
+++ client/src/components/views/VideoDetailPage/Sections/Commnet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Commnet() {
+  return <div>Comment</div>;
+}
+
+export default Commnet;
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
+import Commnet from "./Sections/Commnet";
 
 function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -21,38 +22,46 @@ function VideoDetailPage(props) {
     });
   }, []);
 
-  return (
-    <Row gutter={[16, 16]}>
-      <Col lg={18} xs={24}>
-        <div style={{ width: "100%", padding: "3rem 4rem" }}>
-          <video
-            style={{ width: "100%" }}
-            src={`http://localhost:5000/${Video.filePath}`}
-            controls
-          />
+  if (Video.writer) {
+    console.log(Video);
 
-          <List.Item
-            actions={[
-              <Subscribe
-                userTo={Video.writer_id}
-                userFrom={localStorage.getItem("userId")}
-              />,
-            ]}
-          >
-            <List.Item.Meta
-              avatar={<Avatar src={Video.writer && Video.writer.image} />}
-              title={<a href="https://ant.design">{Video.title}</a>}
-              description={Video.description}
+    const subscribeBtn = Video.writer_id !== localStorage.getItem("userId") && (
+      <Subscribe
+        userTo={Video.writer_id}
+        userFrom={localStorage.getItem("userId")}
+      />
+    );
+
+    return (
+      <Row gutter={[16, 16]}>
+        <Col lg={18} xs={24}>
+          <div style={{ width: "100%", padding: "3rem 4rem" }}>
+            <video
+              style={{ width: "100%" }}
+              src={`http://localhost:5000/${Video.filePath}`}
+              controls
             />
-            <div></div>
-          </List.Item>
-        </div>
-      </Col>
-      <Col lg={6} xs={24}>
-        <SideVideo />
-      </Col>
-    </Row>
-  );
+
+            <List.Item actions={[subscribeBtn]}>
+              <List.Item.Meta
+                avatar={<Avatar src={Video.writer && Video.writer.image} />}
+                title={<a href="https://ant.design">{Video.title}</a>}
+                description={Video.description}
+              />
+              <div></div>
+            </List.Item>
+
+            <Commnet />
+          </div>
+        </Col>
+        <Col lg={6} xs={24}>
+          <SideVideo />
+        </Col>
+      </Row>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default VideoDetailPage;
